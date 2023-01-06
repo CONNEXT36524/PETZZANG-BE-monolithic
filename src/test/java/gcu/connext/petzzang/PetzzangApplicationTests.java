e592a415328d97b99631ded8b5883bdf4d4b85be
Dajeong Park <jul897813@gmail.com>
2023-01-06 14:41:03 +0900

    first commit

---
+++ src/test/java/gcu/connext/petzzang/PetzzangApplicationTests.java
@@ -0,0 +1,13 @@
+package gcu.connext.petzzang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetzzangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
