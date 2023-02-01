38bd64bb867113910b29769e476f075a2d960b2b
HM <climatc1103@naver.com>
2023-02-01 14:42:29 +0900

    feat: add schedule

--- src/main/java/gcu/connext/petzzang/community/repository/PostRepository.java
+++ src/main/java/gcu/connext/petzzang/community/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package gcu.connext.petzzang.community.repository;
 
 import gcu.connext.petzzang.community.entity.Post;
+import gcu.connext.petzzang.ranking.entity.Weekly_ranking_entity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
@@ -10,4 +16,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Post findByBoardType(Long boardType);
 
     Post findByPostId(Long postCode);
+
+
+        @Query(value = "select * from posts_tb where create_time>=DATE_SUB(NOW(),INTERVAL (DAYOFWEEK(NOW())-1) DAY) ORDER BY views DESC", nativeQuery = true)
+        public List<Post> finddate();
+
 }
--- src/main/java/gcu/connext/petzzang/ranking/service/rankingschedule.java
+++ src/main/java/gcu/connext/petzzang/ranking/service/rankingschedule.java
@@ -1,19 +1,30 @@
 package gcu.connext.petzzang.ranking.service;
 
+import gcu.connext.petzzang.community.entity.Post;
+import gcu.connext.petzzang.community.repository.PostRepository;
 import gcu.connext.petzzang.ranking.repository.Weekly_ranking_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 @Component
 public class rankingschedule {
 
+    @Autowired
     private Weekly_ranking_Repository weekly_ranking_repository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Async
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(fixedRate = 3600000)
 // @Scheduled(fixedRateString = "${fixedRate.in.milliseconds}")  // 문자열 milliseconds 사용 시
     public void scheduleFixedRateTask() throws InterruptedException {
-
+        List<Post> post=postRepository.finddate();
+        System.out.println(post);
     }
 }
