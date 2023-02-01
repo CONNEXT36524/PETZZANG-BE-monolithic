9e3b48fe9e10989bf38890e16d54fb438817b57c
Dajeong Park <jul897813@gmail.com>
2023-02-01 10:44:52 +0900

    refactor: upload monolithic BE

---
+++ src/main/java/gcu/connext/petzzang/PetzzangApplication.java
@@ -0,0 +1,13 @@
+package gcu.connext.petzzang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetzzangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetzzangApplication.class, args);
+	}
+
+}
