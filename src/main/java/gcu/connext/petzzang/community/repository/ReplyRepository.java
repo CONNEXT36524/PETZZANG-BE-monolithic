110a1d6fdd46fc6fde81f6ec0a3adb459db5a79d
Dajeong Park <jul897813@gmail.com>
2023-02-03 13:53:38 +0900

    feat: GetMapping ReplyService / getReplies

--- src/main/java/gcu/connext/petzzang/community/repository/ReplyRepository.java
+++ src/main/java/gcu/connext/petzzang/community/repository/ReplyRepository.java
@@ -3,10 +3,12 @@ package gcu.connext.petzzang.community.repository;
 import gcu.connext.petzzang.community.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     // JPA findBy 규칙
     // select * from reply_master where kakao_email = ?
 
-    Reply findByPostId(Long postId);
+    List<Reply> findByPostId(Long postId);
 }
