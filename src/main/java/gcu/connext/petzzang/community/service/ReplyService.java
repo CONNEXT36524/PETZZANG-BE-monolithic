d0620f327ef5cb13e6b7ab5f5d65fdfadc1ea6e3
Dajeong Park <jul897813@gmail.com>
2023-02-15 01:41:12 +0900

    feat: Add ReplyIdComparator
    to get replies

--- src/main/java/gcu/connext/petzzang/community/service/ReplyService.java
+++ src/main/java/gcu/connext/petzzang/community/service/ReplyService.java
@@ -20,14 +20,14 @@ public class ReplyService {
     @Transactional
     public List<Reply> getReply(Integer postId) {
         List<Reply> replyList = replyRepository.findByPostId(Long.valueOf(postId));
-
+        Collections.sort(replyList, new ReplyIdComparator());
         return replyList;
     }
 
     @Transactional
     public List<Reply> getNReply(Integer postId, Integer bundleId) {
         List<Reply> replyList = replyRepository.findByPostIdAndBundleId(Long.valueOf(postId), Long.valueOf(bundleId));
-        System.out.println("replylist--" + replyList);
+        Collections.sort(replyList, new ReplyIdComparator());
         return replyList;
     }
 
@@ -36,7 +36,6 @@ public class ReplyService {
         List<Reply> replyList = replyRepository.findByPostId(Long.valueOf(reply.getPostId()));
         if (replyList.size() != 0)
         {
-
             Collections.sort(replyList, new ReplyBundleIdComparator());
             Long latestBundleId = replyList.get(replyList.size() -1).getBundleId();
             reply.setBundleId(latestBundleId +  1);
@@ -49,14 +48,26 @@ public class ReplyService {
         Long postId = reply.getPostId();
         Long bundleId = reply.getBundleId();
         List<Reply> replyList = replyRepository.findByPostIdAndBundleId(postId, bundleId);
-        if (replyList.size() != 0) {
-
+        if (replyList.size() != 0)
+        {
             Collections.sort(replyList, new ReplyBundleOrderComparator());
             Long latestBundleOrder = replyList.get(replyList.size() - 1).getBundleOrder();
             reply.setBundleOrder(latestBundleOrder + 1);
         }
             return replyRepository.save(reply);
 
+    }
+
+    class ReplyIdComparator implements Comparator<Reply> {
+        @Override
+        public int compare(Reply r1, Reply r2) {
+            if (r1.getReplyId() > r2.getReplyId()) {
+                return 1;
+            } else if (r1.getReplyId() < r2.getReplyId()) {
+                return -1;
+            }
+            return 0;
+        }
     }
 
     class ReplyBundleIdComparator implements Comparator<Reply> {
