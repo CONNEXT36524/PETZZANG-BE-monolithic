94ad035c8d4765d6587b72092e1407fcb4b59730
Dajeong Park <jul897813@gmail.com>
2023-02-20 20:22:41 +0900

    fix: upload image to objectStorage

--- src/main/java/gcu/connext/petzzang/community/controller/PostingController.java
+++ src/main/java/gcu/connext/petzzang/community/controller/PostingController.java
@@ -5,8 +5,16 @@ import gcu.connext.petzzang.community.entity.Post;
 import gcu.connext.petzzang.community.service.PostingService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
+import org.springframework.util.Base64Utils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.awt.*;
+import java.math.BigInteger;
+import java.util.Base64;
 
 
 @RestController
@@ -23,7 +31,6 @@ public class PostingController {
     @ResponseStatus(HttpStatus.CREATED)
     public Post createPosting(
             @ModelAttribute PostDTO postDTO
-            // @RequestParam("files") List<MultipartFile> files
     ) throws Exception {
 
         System.out.println(postDTO.thumbnail);
@@ -32,10 +39,32 @@ public class PostingController {
         //DTO to Entity
         Post post = modelMapper.map(postDTO, Post.class);
 
-        //save in mysql database
-        postingService.uploadPosting(post);
+        // save in mysql database
+        // postingService.uploadPosting(post);
         //URI uriLocation = new URI("/board/" + board.getID());
         return postingService.uploadPosting(post);
     }
 
+    @PostMapping("/image")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<byte[]> uploadImg(
+            @RequestParam(name="imgFile") String key
+    ) throws Exception {
+
+        RestTemplate rt = new RestTemplate();
+
+        String keyBase64 = key.substring(22);
+        byte[] decodedBytes = Base64.getMimeDecoder().decode(keyBase64);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Auth-Token", "token");
+        headers.add("Content-Type", "image/png");
+
+        HttpEntity<byte[]> entity = new HttpEntity<>(decodedBytes, headers);
+
+        String url = "https://objectstorage.kr-central-1.kakaoi.io/v1/cbfb40eb783145cbbc2fec56fd713fd3/pz-os/thumbnail/real2.png";
+
+        return rt.exchange(url, HttpMethod.PUT, entity, byte[].class);
+
+    }
 }
--- src/main/java/gcu/connext/petzzang/community/service/PostingService.java
+++ src/main/java/gcu/connext/petzzang/community/service/PostingService.java
@@ -4,7 +4,14 @@ import gcu.connext.petzzang.community.dto.PostDTO;
 import gcu.connext.petzzang.community.entity.Post;
 import gcu.connext.petzzang.community.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
 
 @Service
 public class PostingService {
@@ -15,5 +22,23 @@ public class PostingService {
     //mysql에 게시글 저장하기
     public Post uploadPosting(Post post) {
         return postRepository.save(post);
+    }
+
+    public ResponseEntity<byte[]> uploadThumbnail(String key) {
+
+        RestTemplate rt = new RestTemplate();
+
+        String keyBase64 = key.substring(22);
+        byte[] decodedBytes = Base64.getMimeDecoder().decode(keyBase64);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Auth-Token", "gAAAAABj8z-LRWmmLnduDGQcTKFOH--_Rv8msEbtTLzxVIL_1MoX211bx0XbW3VjWHVMchg6i8ydGdOkfm22RVy6DYqq8BMk1E-kFypmAfU3UObwMe9USHrWkVT2VcGy-3lw29sl5WQTc_pvt-EkDDOYhL8UyxoLYsaVkywqMoBpuud8pIJgDUJLhD8iQZCujTSrKy8Tqjdu");
+        headers.add("Content-Type", "image/png");
+
+        HttpEntity<byte[]> entity = new HttpEntity<>(decodedBytes, headers);
+
+        String url = "https://objectstorage.kr-central-1.kakaoi.io/v1/cbfb40eb783145cbbc2fec56fd713fd3/pz-os/thumbnail/real2.png";
+
+        return rt.exchange(url, HttpMethod.PUT, entity, byte[].class);
     }
 }
